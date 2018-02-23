e242a0e2f2b4e9580032bbcca9dcf7ef84016082
elye <elye.project@gmail.com>
2018-02-23 11:42:38 +1100

    Remove Top Fragment

--- app/src/main/java/com/elyeproj/myapplication/MainActivity.kt
+++ app/src/main/java/com/elyeproj/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.elyeproj.myapplication
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,12 +11,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.container, TopFragment()).commitAllowingStateLoss()
+        add_f1.setOnClickListener {
+            addFragment(OneFragment(),  "OneFragment")
+        }
+
+        replace_f1.setOnClickListener {
+            replaceFragment(OneFragment(),  "OneFragment")
+        }
+
+        add_f2.setOnClickListener {
+            addFragment(TwoFragment(),  "TwoFragment")
+        }
+
+        replace_f2.setOnClickListener {
+            replaceFragment(TwoFragment(),  "TwoFragment")
+        }
+
+        add_f3.setOnClickListener {
+            addFragment(ThreeFragment(),  "ThreeFragment")
+        }
+
+        replace_f3.setOnClickListener {
+            replaceFragment(ThreeFragment(),  "ThreeFragment")
+        }
+
+        add_f4.setOnClickListener {
+            addFragment(FourFragment(),  "FourFragment")
+        }
+
+        replace_f4.setOnClickListener {
+            replaceFragment(FourFragment(),  "FourFragment")
+        }
+    }
+
+    private fun addFragment(fragment: Fragment, name: String) {
+        supportFragmentManager.beginTransaction().add(R.id.container, fragment)
+                .addToBackStack(name).commitAllowingStateLoss()
+    }
+
+    private fun replaceFragment(fragment: Fragment, name: String) {
+        supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+                .addToBackStack(name).commitAllowingStateLoss()
     }
 
     override fun onBackPressed() {
-        if (!(supportFragmentManager.findFragmentById(R.id.container) as TopFragment).onBackPressed()) {
+
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStackImmediate()
+        } else {
             super.onBackPressed()
         }
     }
